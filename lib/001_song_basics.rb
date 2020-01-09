2dccf30e429b3474ded05bfcbc1504555b5903d3
nicksolony <nicksolony@gmail.com>
2020-01-08 23:56:16 -0500

    pass all test from 001_song_basics_spec.rb

--- lib/001_song_basics.rb
+++ lib/001_song_basics.rb
@@ -3,12 +3,32 @@ class Song
 
   def initialize (name)
     @name = name
-    @@all << self
   end
+
 
   def name
     @name
   end
 
-  
+  def name= (name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create (name)
+    self.new(name).save
+    self
+  end
+
 end

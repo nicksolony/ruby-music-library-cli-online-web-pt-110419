d505d005903c742fb268ee9754006ae0d20e15b7
nicksolony <nicksolony@gmail.com>
2020-01-09 22:57:19 -0500

    o bogi

--- lib/001_song_basics.rb
+++ lib/001_song_basics.rb
@@ -1,8 +1,17 @@
+require 'pry'
 class Song
 @@all = []
 
-  def initialize (name)
+attr_accessor :artist
+
+  def initialize (name,artist=nil, genre = nil)
     @name = name
+  #  if artist != nil
+  #    @artist =artist
+  #    self.artist = artist
+  #  end
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
   end
 
 
@@ -30,5 +39,26 @@ class Song
     self.new(name).save
     self
   end
+  def artist
+    @artist
+  end
+  def artist=artist
+   # artist = Artist.new
+   @artist = artist
+   artist.add_song(self)
+  end
 
+  def genre
+    @genre
+  end
+
+  def genre=genre
+    @genre = genre
+    genre.add_song(self) unless genre.songs.include?(self)
+  end
+#  binding pry
+
+  def self.find_by_name(name)
+    all.detect {|song| song.name == name}
+  end
 end
